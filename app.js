1faab016d072e8dbbc05134387ccfc93e1fb4240
Sayam Kumar <55996885+Sayam2801@users.noreply.github.com>
2021-03-04 04:04:50 +0530

    modified some files

--- app.js
+++ app.js
@@ -11,32 +11,147 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static("public"));
 
+let storeCountryNames = [];
+
 app.get("/", function(req,res) {
     res.render("index");
 });
 
+// app.get("/countries", function(req,res) {
+//   res.render("allcountries", {storeCountryNames:storeCountryNames});
+// });
+//
+// app.get("/global", function(req,res) {
+//   res.render("global");
+// });
+//
+// app.get("/country", function(req,res) {
+//   res.render("country");
+// });
+// app.get("/country", function(req,res) {
+//   const countryName = _.camelCase(req.body.cName);
+//   const cases = req.body.cases;
+//   const todayCases = req.body.todayCases;
+//   const deaths = req.body.todayCases;
+//   const recovered = req.body.recovered;
+//   const active = req.body.active;
+//   const critical = req.body.critical;
+//   const casesPerOneMillion = req.body.casesPerOneMillion;
+//   const deathsPerOneMillion = req.body.deathsPerOneMillion;
+//   const totalTests = req.body.totalTests;
+//   const testsPerOneMillion = req.body.testsPerOneMillion;
+//   res.render("country",{
+//     name: countryName,
+//     noc: cases,
+//     notc: todayCases,
+//     nod: deaths,
+//     nor: recovered,
+//     noa: active,
+//     nocr: critical,
+//     nocpom: casesPerOneMillion,
+//     nodpom: deathsPerOneMillion,
+//     nott: totalTests,
+//     notpom: testsPerOneMillion
+//   });
+// });
+
 app.post("/records", function(req,res) {
      let url = "https://coronavirus-19-api.herokuapp.com";
-     console.log(url);
      const chosenOption = _.lowerCase(req.body.option);
      const cName = _.camelCase(req.body.cName);
-     console.log(chosenOption);
-     console.log(cName);
-     if(req.body.option === "global")
+     if(chosenOption === "global")
         url=url+"/all";
-     if(req.body.option === "countries")
+     if(chosenOption === "countries")
         url=url+"/countries";
-     if(req.body.option === "country")
+     if(chosenOption === "country")
         url=url+"/countries/"+cName;
-     console.log(url);
-     https.request(url, function(response) {
-       response.on("data", function(data) {
-         const covidData = JSON.parse(data);
-         console.log(covidData);
-       });
+     const request = https.request(url,(response)=>{
+         let data ='';
+         response.on('data',(chunk)=>{
+             data = data + chunk.toString();
+             console.log(chunk);
+         });
+         response.on('end',()=>{
+             const body = JSON.parse(data);
+             console.log(body);
+             for(var i=0;i<body.length;i++){
+               storeCountryNames.push({
+                 name: body[i].country,
+                 noc: body[i].cases,
+                 notc: body[i].todayCases,
+                 nod: body[i].deaths,
+                 nor: body[i].recovered,
+                 noa: body[i].active,
+                 nocr: body[i].critical,
+                 nocpom: body[i].casesPerOneMillion,
+                 nodpom: body[i].deathsPerOneMillion,
+                 nott: body[i].totalTests,
+                 notpom: body[i].testsPerOneMillion
+              });
+            }
+             if(chosenOption === "global"){
+                res.render("global", {
+                  noc: body.cases,
+                  nod: body.deaths,
+                  nor: body.recoveries
+                });
+              }
+              if(chosenOption === "countries")
+                res.render("allcountries", {countryDetails: storeCountryNames });
+              if(chosenOption === "country"){
+                var temp;
+                for(var i=0;i<storeCountryNames.length;i++)
+                {
+                  const storedCName = _.camelCase(storeCountryNames[i].country);
+                  console.log(storedCName);
+                  if(storedCName === cName) {
+                      temp = storeCountryNames[i];
+                      break;
+                  }
+                }
+                console.log(JSON.parse(data));
+                res.render("country", {countryDetails: temp });
+              }
+         });
      });
+     request.on('error', (error)=>{
+         console.log('An error',error);
+     });
+     request.end();
 });
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server is up and running on port 3000.");
 });
+
+// const http = require('http')
+// const url = 'http://api.weatherstack.com/current?access_key=d5e29e33611cdfde58e9992628cebe15&query=37.8267,-122.4233&units=f'
+//
+// const request = http.request(url,(response)=>{
+//     let data =''
+//
+//     response.on('data',(chunk)=>{
+//         data = data + chunk.toString()
+//         console.log(chunk)
+//     })
+//
+//     response.on('end',()=>{
+//         const body = JSON.parse(data)
+//         console.log(body)
+//     })
+// })
+//
+// request.on('error', (error)=>{
+//     console.log('An error',error)
+// })
+//
+// request.end()
+// you can access the final js object where you see the variable body
+// data comes in chunk, so you have to add it manually
+// axios and node-fetch will do this automatically
+// fetch(url)
+//   .then(resp => resp.json())
+//   .then(data => console.log(data))
+//   .catch(e => console.log(e))
+// https://corona-tracker.juggernaut9.now.sh/
+// https://github.com/Juggernaut9/corona-tracker
