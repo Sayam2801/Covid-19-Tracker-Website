777bb243aba63331d053ad5d6bcd08642d606d7a
Sayam Kumar <55996885+Sayam2801@users.noreply.github.com>
2021-03-03 23:37:10 +0530

    Second Commit

--- app.js
+++ app.js
@@ -4,25 +4,37 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const https = require("https");
-const axios = require("axios").default;
+const _ = require("lodash");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
-app.set(express.static("public"));
+app.use(express.static("public"));
 
 app.get("/", function(req,res) {
-    res.sendFile(__dirname + "/index.html");
+    res.render("index");
 });
 
-app.get("/records", function(req,res) {
-  // if(req.body.recordType === "global")
-  //     res.render("global");
-  // if(req.body.recordType === "countries")
-  //     res.render("countries");
-  // if(req.body.recordType === "country")
-  //     res.render("country");
-  console.log(req.body);
+app.post("/records", function(req,res) {
+     let url = "https://coronavirus-19-api.herokuapp.com";
+     console.log(url);
+     const chosenOption = _.lowerCase(req.body.option);
+     const cName = _.camelCase(req.body.cName);
+     console.log(chosenOption);
+     console.log(cName);
+     if(req.body.option === "global")
+        url=url+"/all";
+     if(req.body.option === "countries")
+        url=url+"/countries";
+     if(req.body.option === "country")
+        url=url+"/countries/"+cName;
+     console.log(url);
+     https.request(url, function(response) {
+       response.on("data", function(data) {
+         const covidData = JSON.parse(data);
+         console.log(covidData);
+       });
+     });
 });
 
 app.listen(process.env.PORT || 3000, function() {
